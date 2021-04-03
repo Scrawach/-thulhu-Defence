2de357cd62bb23e2602f6d7a6b0ab07f8e77090a
scraw <seenows@gmail.com>
2021-04-03 21:38:30 +0300

    Added purple coin in prefab & fixed some text bugs

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField] 
+    private float _speed;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, _speed * Time.deltaTime);
+    }
+}

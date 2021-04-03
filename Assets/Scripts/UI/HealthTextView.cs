a34e17853f2b333c4626a93e447b373e809cad55
scraw <seenows@gmail.com>
2021-04-03 23:54:38 +0300

    Global reedit UI

---
+++ Assets/Scripts/UI/HealthTextView.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using EnemyLogic;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class HealthTextView : MonoBehaviour
+    {
+        public Health Health;
+        public TextMeshProUGUI View;
+        public CanvasFade Fade;
+
+        private Coroutine _coroutine;
+        
+        private void OnEnable() => 
+            Health.Changed += OnHealthChanged;
+
+        private void OnDisable() => 
+            Health.Changed -= OnHealthChanged;
+
+        private void OnHealthChanged(float arg1, float arg2)
+        {
+            View.text = arg1.ToString();
+            Fade.Show();
+
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+            
+            _coroutine = StartCoroutine(Waiting());
+        }
+
+        private IEnumerator Waiting()
+        {
+            yield return new WaitForSeconds(5f);
+            Fade.Hide();
+        }
+    }
+}
